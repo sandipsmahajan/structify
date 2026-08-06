1ab13dd54fa1ca2ddd3ad4f5a9a7e436b622ca1b
Sandip Mahajan <42683645+sandipsmahajan@users.noreply.github.com>
2026-08-06 20:22:51 +0530

    Code deleted

--- src/app/big-o-lab/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AppShell } from "@/components/layout/AppShell";
-import { ComplexityLab } from "@/components/visualization/ComplexityLab";
-
-export default function BigOLabPage() {
-  return <AppShell><div className="mx-auto max-w-7xl px-4 py-10"><ComplexityLab /></div></AppShell>;
-}
