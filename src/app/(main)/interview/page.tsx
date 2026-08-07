d71c4df2c07cb7efb32aea5f6c308fc831cf978b
Sandip Mahajan <42683645+sandipsmahajan@users.noreply.github.com>
2026-08-08 01:15:20 +0530

    Deleted the code

--- src/app/(main)/interview/page.tsx
+++
@@ -1,9 +0,0 @@
-import { InterviewSession } from "@/components/interview/interview-session";
-
-export default async function InterviewPage() {
-  return (
-    <div className="pt-24 pb-16 px-6 min-h-screen">
-      <InterviewSession />
-    </div>
-  );
-}
--- src/middleware.ts
+++
@@ -1,5 +0,0 @@
-export { auth as middleware } from "@/lib/auth-edge";
-
-export const config = {
-  matcher: [],
-};
